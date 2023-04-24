7eb0ddcec45461a3a9af1ced8d2acbebcc78ba1c
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-04-24 15:08:39 -0500

    [add] - Initial commit 🚀

---
+++ Lists/ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsApp.swift
+//  Lists
+//
+//  Created by Pierre Juarez U. on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
